ae0ef50ccbd9545be15b6e02253f65134446c35a
常亮 <changliang@ftsafe.com>
2022-05-20 17:43:23 +0800

    verify message

--- src/wallet.ts
+++ src/wallet.ts
@@ -1,31 +1,35 @@
 import { SM2 } from "gm-ts";
-import { ec } from 'elliptic';
+import { ec ,SignatureInput} from 'elliptic';
 import { mnemonicToSeedSync } from 'bip39';
 
 export class HyperWallet {
   keyPair: ec.KeyPair;
-  sm2:SM2;
+  sm2: SM2;
   constructor(privateKey: string) {
     this.sm2 = new SM2();
     this.keyPair = this.sm2.keyFromPrivate(privateKey);
   }
 
   sign(msg: string) {
-    const sig =  this.sm2.sign(Buffer.from(msg,"hex"),this.keyPair);
-    return sig.r.toString("hex") + sig.s.toString("hex");
+    const sig = this.sm2.sign(Buffer.from(msg, "hex"), this.keyPair);
+    return sig.toDER("hex");
   }
 
-  getPublicKey():string{
-      return this.keyPair.getPublic("hex").toString();
+  verify(msg: string, signature: string): boolean {
+    return this.sm2.verify(Buffer.from(msg, "hex"), Buffer.from(signature,"hex") , this.keyPair);
+  }
+
+  getPublicKey(): string {
+    return this.keyPair.getPublic("hex").toString();
   }
 }
 
-export function newWalletFromPrivateKey(privateKey:string){
-    return new HyperWallet(privateKey);
+export function newWalletFromPrivateKey(privateKey: string) {
+  return new HyperWallet(privateKey);
 }
 
-export function newWalletFromMnemonic(mnemonic:string){
-    const seed = mnemonicToSeedSync(mnemonic);
-    const privateKey = seed.slice(0,32);
-    return new HyperWallet(privateKey.toString("hex"));
+export function newWalletFromMnemonic(mnemonic: string) {
+  const seed = mnemonicToSeedSync(mnemonic);
+  const privateKey = seed.slice(0, 32);
+  return new HyperWallet(privateKey.toString("hex"));
 }
